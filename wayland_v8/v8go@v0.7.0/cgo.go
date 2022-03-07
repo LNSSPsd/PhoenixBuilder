19d58eb624b1c54644b7392786aa05f0e7cb09a6
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-03-07 18:43:26 +0800

    Auto select flags

--- wayland_v8/v8go@v0.7.0/cgo.go
+++ wayland_v8/v8go@v0.7.0/cgo.go
@@ -6,14 +6,18 @@ package v8go
 
 //go:generate clang-format -i --verbose -style=Chromium v8go.h v8go.cc
 
-//#cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH -I${SRCDIR}/deps/include -Wall
+//#cgo CXXFLAGS: -fno-rtti -fpic -std=c++14 -I${SRCDIR}/deps/include -Wall
 //#cgo LDFLAGS: -pthread -lv8
 //#cgo android,arm64 LDFLAGS: -L${SRCDIR}/deps/android_arm64
 //#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/darwin_x86_64
+//#cgo darwin,amd64 CXXFLAGS: -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
 //#cgo darwin,arm64,!ios LDFLAGS: -L${SRCDIR}/deps/darwin_arm64
+//#cgo darwin,arm64,!ios CXXFLAGS: -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
 //#cgo ios,arm64 LDFLAGS: -L${SRCDIR}/deps/ios_arm64 -lc++ -lc++abi
 //#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/linux_x86_64 -ldl
+//#cgo linux,amd64 CXXFLAGS: -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
 //#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/linux_arm64 -ldl
+//#cgo linux,arm64 CXXFLAGS: -DV8_COMPRESS_POINTERS -DV8_31BIT_SMIS_ON_64BIT_ARCH
 import "C"
 
 // These imports forces `go mod vendor` to pull in all the folders that
