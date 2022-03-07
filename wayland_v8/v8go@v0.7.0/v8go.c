54ab0b54730462ba967bb4e19b7bbb22d2aa9a0e
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2022-03-07 18:43:26 +0800

    Auto select flags

--- wayland_v8/v8go@v0.7.0/v8go.c
+++ wayland_v8/v8go@v0.7.0/v8go.c
@@ -1,4 +1,4 @@
 // +build ios
 
-extern "C" void _ZN2v88internal12trap_handler15TryHandleSignalEiP9__siginfoPv() {}
-extern "C" void _ZN2v88internal12trap_handler26RegisterDefaultTrapHandlerEv() {}
+void _ZN2v88internal12trap_handler15TryHandleSignalEiP9__siginfoPv() {}
+void _ZN2v88internal12trap_handler26RegisterDefaultTrapHandlerEv() {}
