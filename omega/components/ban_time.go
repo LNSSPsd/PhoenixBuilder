1043735344a3f9a65a5ef8be84239caf69ae33c0
Liliya233 <abc572024594@gmail.com>
2023-01-09 20:18:02 +0800

    代码优化

--- omega/components/ban_time.go
+++ omega/components/ban_time.go
@@ -15,30 +15,30 @@ import (
 
 type BanTime struct {
 	*defines.BasicComponent
-	Selector             string  `json:"选择器"`
-	Duration             float64 `json:"检查周期"`
-	OnOmegaTakeOver      []defines.Cmd
+	Selector             string      `json:"选择器"`
+	Duration             float64     `json:"检查周期"`
 	OnOmegaTakeOverIn    interface{} `json:"Omega接管时指令"`
-	KickCmd              []defines.Cmd
 	KickCmdIn            interface{} `json:"踢出指令"`
-	AfterOmegaTakeOver   []defines.Cmd
 	AfterOmegaTakeOverIn interface{} `json:"到达封禁时间Omega结束接管时指令"`
 	ScoreboardName       string      `json:"读取封禁时间的计分板名"`
 	FileName             string      `json:"文件名"`
-	mu                   sync.Mutex
-	KickDelay            int `json:"延迟踢出时间"`
-	LoginDelay           int `json:"登录时延迟发送"`
+	KickDelay            int         `json:"延迟踢出时间"`
+	LoginDelay           int         `json:"登录时延迟发送"`
+	OnOmegaTakeOver      []defines.Cmd
+	KickCmd              []defines.Cmd
+	AfterOmegaTakeOver   []defines.Cmd
 	banTime              map[string]time.Time
 	banTimeStr           map[string]string
 	fileChange           bool
+	mu                   sync.Mutex
 }
 
 func (o *BanTime) Init(cfg *defines.ComponentConfig) {
 	m, _ := json.Marshal(cfg.Configs)
-	if err := json.Unmarshal(m, o); err != nil {
+	var err error
+	if err = json.Unmarshal(m, o); err != nil {
 		panic(err)
 	}
-	var err error
 	if o.OnOmegaTakeOver, err = utils.ParseAdaptiveCmd(o.OnOmegaTakeOverIn); err != nil {
 		panic(err)
 	}
@@ -49,12 +49,12 @@ func (o *BanTime) Init(cfg *defines.ComponentConfig) {
 		panic(err)
 	}
 	o.mu = sync.Mutex{}
+	o.banTime = make(map[string]time.Time)
+	o.banTimeStr = make(map[string]string)
 }
 
 func (o *BanTime) Inject(frame defines.MainFrame) {
 	o.Frame = frame
-	o.banTime = make(map[string]time.Time)
-	o.banTimeStr = make(map[string]string)
 	err := frame.GetJsonData(o.FileName, &o.banTimeStr)
 	if err != nil {
 		panic(err)
@@ -67,42 +67,37 @@ func (o *BanTime) Inject(frame defines.MainFrame) {
 	}
 	o.Frame.GetGameListener().AppendLoginInfoCallback(func(entry protocol.PlayerListEntry) {
 		o.mu.Lock()
-		if banTime, hasK := o.banTime[entry.Username]; !hasK {
-			o.mu.Unlock()
-		} else {
+		defer o.mu.Unlock()
+		if banTime, hasK := o.banTime[entry.Username]; hasK {
 			if banTime.After(time.Now()) {
-				o.mu.Unlock()
 				go func() {
 					<-time.NewTimer(time.Duration(o.KickDelay) * time.Second).C
 					o.kick(entry.Username)
 				}()
-
 			} else {
 				delete(o.banTime, entry.Username)
 				delete(o.banTimeStr, entry.Username)
 				o.fileChange = true
-				o.mu.Unlock()
 				go func() {
 					<-time.NewTimer(time.Duration(o.LoginDelay) * time.Second).C
 					utils.LaunchCmdsArray(o.Frame.GetGameControl(), o.AfterOmegaTakeOver, map[string]interface{}{
 						"[player]": entry.Username,
 					}, o.Frame.GetBackendDisplay())
 				}()
-
 			}
 		}
 	})
 }
 
-func (o *BanTime) Signal(signal int) error {
+func (o *BanTime) Signal(signal int) (err error) {
 	switch signal {
 	case defines.SIGNAL_DATA_CHECKPOINT:
 		if o.fileChange {
+			err = o.Frame.WriteJsonDataWithTMP(o.FileName, ".ckpt", o.banTimeStr)
 			o.fileChange = false
-			return o.Frame.WriteJsonDataWithTMP(o.FileName, ".ckpt", o.banTimeStr)
 		}
 	}
-	return nil
+	return err
 }
 
 func (o *BanTime) Stop() error {
@@ -111,27 +106,18 @@ func (o *BanTime) Stop() error {
 }
 
 func (o *BanTime) kick(name string) {
-	s, m, h, d := "?", "?", "?", "?"
 	o.mu.Lock()
-	if banTime, hasK := o.banTime[name]; !hasK {
-		o.mu.Unlock()
-		return
-	} else {
-		o.mu.Unlock()
-		duration := banTime.Sub(time.Now())
-		s = fmt.Sprintf("%v", int(duration.Seconds())%60)
-		m = fmt.Sprintf("%v", int(duration.Minutes())%60)
-		h = fmt.Sprintf("%v", int(duration.Hours())%24)
-		d = fmt.Sprintf("%v", int(duration.Hours())/24)
+	defer o.mu.Unlock()
+	if banTime, hasK := o.banTime[name]; hasK {
+		duration := time.Until(banTime)
+		go utils.LaunchCmdsArray(o.Frame.GetGameControl(), o.KickCmd, map[string]interface{}{
+			"[player]": name,
+			"[day]":    int(duration.Hours()) / 24,
+			"[hour]":   int(duration.Hours()) % 24,
+			"[min]":    int(duration.Minutes()) % 60,
+			"[sec]":    int(duration.Seconds()) % 60,
+		}, o.Frame.GetBackendDisplay())
 	}
-	go utils.LaunchCmdsArray(o.Frame.GetGameControl(), o.KickCmd, map[string]interface{}{
-		"[player]": name,
-		"[day]":    d,
-		"[hour]":   h,
-		"[min]":    m,
-		"[sec]":    s,
-	}, o.Frame.GetBackendDisplay())
-
 }
 
 func (o *BanTime) takeOver(name string) {
@@ -139,7 +125,7 @@ func (o *BanTime) takeOver(name string) {
 		if err != nil {
 			pterm.Error.Printfln("无法获取封禁时间信息 %v %v %v", name, o.ScoreboardName, err)
 		} else if val < 0 {
-			pterm.Error.Printfln("封禁时间指令设计配置有问题，如果封禁时间小于等于 0，则不应该被选择器选中 %v %v %v", name, o.ScoreboardName)
+			pterm.Error.Printfln("封禁时间指令设计配置有问题，如果封禁时间小于等于 0，则不应该被选择器选中 %v %v", name, o.ScoreboardName)
 		} else {
 			duration := time.Second * time.Duration(val)
 			banTime := time.Now().Add(duration)
@@ -149,23 +135,18 @@ func (o *BanTime) takeOver(name string) {
 			o.mu.Unlock()
 			o.fileChange = true
 			go func() {
-				s := fmt.Sprintf("%v", int(duration.Seconds())%60)
-				m := fmt.Sprintf("%v", int(duration.Minutes())%60)
-				h := fmt.Sprintf("%v", int(duration.Hours())%24)
-				d := fmt.Sprintf("%v", int(duration.Hours())/24)
 				utils.LaunchCmdsArray(o.Frame.GetGameControl(), o.OnOmegaTakeOver, map[string]interface{}{
 					"[player]": name,
-					"[day]":    d,
-					"[hour]":   h,
-					"[min]":    m,
-					"[sec]":    s,
+					"[day]":    int(duration.Hours()) / 24,
+					"[hour]":   int(duration.Hours()) % 24,
+					"[min]":    int(duration.Minutes()) % 60,
+					"[sec]":    int(duration.Seconds()) % 60,
 				}, o.Frame.GetBackendDisplay())
 				o.kick(name)
 			}()
 		}
 	})
 }
-
 func (o *BanTime) Activate() {
 	t := time.NewTicker(time.Second * time.Duration(o.Duration))
 	for {
