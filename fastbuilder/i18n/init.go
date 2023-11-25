58018d84a3511ea35a6307399864f5b19dfa27e9
Happy2018new <chenbo79800@163.com>
2023-11-25 21:19:46 +0800

    some minor changes

--- fastbuilder/i18n/init.go
+++ fastbuilder/i18n/init.go
@@ -27,29 +27,27 @@ func Init() {
 	}
 	langdict, aru := LangDict[SelectedLanguage]
 	is_deprecated := IsDeprecated(SelectedLanguage)
-	if is_deprecated || !aru {
-		if is_deprecated {
-			pterm.Warning.Printf("The language named `%s` has been removed and will be redirected to `%s`.\nPress Enter to continue.\n", SelectedLanguage, DefaultLanguage)
-			langdict = LangDict["en_US"]
-			if file, err := os.Create(config); err != nil {
-				fmt.Println(T(Lang_Config_ErrOnCreate), err)
-				fmt.Println(T(ErrorIgnored))
-			} else {
-				_, err = file.WriteString(DefaultLanguage)
-				if err != nil {
-					fmt.Println(T(Lang_Config_ErrOnSave), err)
-					fmt.Println(T(ErrorIgnored))
-				}
-				file.Close()
-			}
-			bufio.NewReader(os.Stdin).ReadString('\n')
+	if is_deprecated {
+		pterm.Warning.Printf("The language named `%s` has been removed and will be redirected to `%s`.\nPress Enter to continue.\n", SelectedLanguage, DefaultLanguage)
+		langdict = LangDict["en_US"]
+		if file, err := os.Create(config); err != nil {
+			fmt.Println(T(Lang_Config_ErrOnCreate), err)
+			fmt.Println(T(ErrorIgnored))
 		} else {
-			fmt.Printf("Ordered language doesn't exist.\nPlease reselect one:\n")
-			SelectLanguage()
-			langdict, aru = LangDict[SelectedLanguage]
-			if !aru {
-				panic("Language still unexists after reselection")
+			_, err = file.WriteString(DefaultLanguage)
+			if err != nil {
+				fmt.Println(T(Lang_Config_ErrOnSave), err)
+				fmt.Println(T(ErrorIgnored))
 			}
+			file.Close()
+		}
+		bufio.NewReader(os.Stdin).ReadString('\n')
+	} else if !aru {
+		fmt.Printf("Ordered language doesn't exist.\nPlease reselect one:\n")
+		SelectLanguage()
+		langdict, aru = LangDict[SelectedLanguage]
+		if !aru {
+			panic("Language still unexists after reselection")
 		}
 	}
 	I18nDict = langdict
