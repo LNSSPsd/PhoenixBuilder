542e9d5cc85461e4ecc349667d57968966b81ad9
我不是Art <101685021+Inotart@users.noreply.github.com>
2024-03-10 22:06:11 +0800

    Update setblock.go
    修复解析Bug 生成的 /setblock 的 数字id错误。

--- fastbuilder/commands_generator/setblock.go
+++ fastbuilder/commands_generator/setblock.go
@@ -3,6 +3,7 @@ package commands_generator
 import (
 	"fmt"
 	"phoenixbuilder/fastbuilder/types"
+	"strconv"
 )
 
 func SetBlockRequest(module *types.Module, config *types.MainConfig) string {
@@ -10,13 +11,28 @@ func SetBlockRequest(module *types.Module, config *types.MainConfig) string {
 	Point := module.Point
 	Method := config.Method
 	if Block != nil {
-		if len(Block.BlockStates)!=0 {
-			return fmt.Sprintf("setblock %d %d %d %s %s %s", Point.X, Point.Y, Point.Z, *Block.Name, Block.BlockStates, Method)
-		} else {
+		if Block.BlockStates == "" {
 			return fmt.Sprintf("setblock %d %d %d %s %d %s", Point.X, Point.Y, Point.Z, *Block.Name, Block.Data, Method)
+		} else if Block.BlockStates == "[]" {
+			return fmt.Sprintf("setblock %d %d %d %s %d %s", Point.X, Point.Y, Point.Z, *Block.Name, Block.Data, Method)
+
+		} else {
+			if IsNum(Block.BlockStates) {
+				return fmt.Sprintf("setblock %d %d %d %s %d %s", Point.X, Point.Y, Point.Z, *Block.Name, Block.Data, Method)
+			} else {
+				return fmt.Sprintf("setblock %d %d %d %s %s %s", Point.X, Point.Y, Point.Z, *Block.Name, Block.BlockStates, Method)
+			}
+
 		}
 	} else {
 		return fmt.Sprintf("setblock %d %d %d %s %d %s", Point.X, Point.Y, Point.Z, config.Block.Name, config.Block.Data, Method)
 	}
 
+}
+
+// 这随便写的一个临时函数,随便换个位置都行,偷个懒
+// Art 最喜欢摸鱼了
+func IsNum(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
 }
